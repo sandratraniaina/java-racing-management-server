86558ae3ce981cc1a57fa0d152a309fd71ff6246
sandratraniaina <sandratrarafa@gmail.com>
2024-05-23 16:56:55 +0300

    feat: Update all id to varchar associated with each sequence

--- db/schema.sql
+++ db/schema.sql
@@ -1,54 +1,54 @@
 CREATE TABLE "constructor" (
-    "id" INT PRIMARY KEY auto_increment,
+    "id" VARCHAR(10) DEFAULT ('CON') || LPAD(nextval('constructor_sequence')::TEXT,4,'0') PRIMARY KEY,
     "name" VARCHAR(32),
-    "country" VARCHAR(32) DEFAULT "International"
+    "country" VARCHAR(32) DEFAULT 'International'
 );
 
 CREATE TABLE "nation" (
-    "id" INT PRIMARY KEY auto_increment,
+    "id" VARCHAR(10) DEFAULT ('NAT') || LPAD(nextval('nation_sequence')::TEXT,4,'0') PRIMARY KEY,
     "name" VARCHAR(32)
 );
 
 CREATE TABLE "category" (
-    "id" INT PRIMARY KEY auto_increment,
+    "id" VARCHAR(10) DEFAULT ('CAT') || LPAD(nextval('category_sequence')::TEXT,4,'0') PRIMARY KEY,
     "name" VARCHAR(4)
 );
 
 CREATE TABLE "driver" (
-    "id" INT PRIMARY KEY auto_increment,
+    "id" VARCHAR(10) DEFAULT ('DRI') || LPAD(nextval('driver_sequence')::TEXT,4,'0') PRIMARY KEY,
     "first_name" VARCHAR(16),
     "last_name" VARCHAR(32),
-    "nation_id" INT,
-    "constructor_id" INT,
-    "category_id" INT,
+    "nation_id" VARCHAR(10),
+    "constructor_id" VARCHAR(10),
+    "category_id" VARCHAR(10),
     FOREIGN KEY ("nation_id") REFERENCES "nation" ("id"),
     FOREIGN KEY ("constructor_id") REFERENCES "constructor" ("id"),
     FOREIGN KEY ("category_id") REFERENCES "category" ("id")
 );
 
 CREATE TABLE "rally" (
-    "id" INT PRIMARY KEY auto_increment,
+    "id" VARCHAR(10) DEFAULT ('RAL') || LPAD(nextval('rally_sequence')::TEXT,4,'0') PRIMARY KEY,
     "name" VARCHAR(16),
     "start_date" DATE DEFAULT null,
     "end_date" DATE DEFAULT null,
-    "location" VARCHAR(16) DEFAULT "Default Location"
+    "location" VARCHAR(16) DEFAULT 'Default Location'
 );
 
-CREATE TABLE stage (
-    "id" INT PRIMARY KEY auto_increment,
-    "rally_id" INT,
+CREATE TABLE "stage" (
+    "id" VARCHAR(10) DEFAULT ('STA') || LPAD(nextval('stage_sequence')::TEXT,4,'0') PRIMARY KEY,
+    "rally_id" VARCHAR(10),
     "stage_number" INT,
     "name" VARCHAR(32),
     "length" FLOAT,
-    "surface" VARCHAR(16) DEFAULT "Default Surface",
+    "surface" VARCHAR(16) DEFAULT 'Default Surface',
     FOREIGN KEY ("rally_id") REFERENCES "rally" ("id")
 );
 
-CREATE TABLE result (
-    "id" INT PRIMARY KEY auto_increment,
-    "rally_id" INT,
-    "driver_id" INT,
-    "stage_id" INT,
+CREATE TABLE "result" (
+    "id" VARCHAR(10) DEFAULT ('RES') || LPAD(nextval('result_sequence')::TEXT,4,'0') PRIMARY KEY,
+    "rally_id" VARCHAR(10),
+    "driver_id" VARCHAR(10),
+    "stage_id" VARCHAR(10),
     "time_s" INT,
     "time_millis" INT,
     FOREIGN KEY ("rally_id") REFERENCES "rally" ("id"),
