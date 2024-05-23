6fdd396e7af9c7710361453c5de2860ac1ec9788
sandratraniaina <sandratrarafa@gmail.com>
2024-05-23 22:12:30 +0300

    feat: Create Result model base class

---
+++ src/model/Result.java
@@ -0,0 +1,59 @@
+package model;
+
+public class Result {
+    String id;
+    String rallyId;
+    String driverId;
+    String stageId;
+    int timeSecond;
+    int timeMillis;
+
+    // Constructor
+    public Result() {}
+    public Result(String id, String rallyId, String driverId, String stageId, int timeSecond, int timeMillis) {
+        setId(id);
+        setRallyId(rallyId);
+        setDriverId(driverId);
+        setStageId(stageId);
+        setTimeSecond(timeSecond);
+        setTimeMillis(timeMillis);  
+    }
+
+    // Getters and setters
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getRallyId() {
+        return rallyId;
+    }
+    public void setRallyId(String rallyId) {
+        this.rallyId = rallyId;
+    }
+    public String getDriverId() {
+        return driverId;
+    }
+    public void setDriverId(String driverId) {
+        this.driverId = driverId;
+    }
+    public String getStageId() {
+        return stageId;
+    }
+    public void setStageId(String stageId) {
+        this.stageId = stageId;
+    }
+    public int getTimeSecond() {
+        return timeSecond;
+    }
+    public void setTimeSecond(int timeSecond) {
+        this.timeSecond = timeSecond;
+    }
+    public int getTimeMillis() {
+        return timeMillis;
+    }
+    public void setTimeMillis(int timeMillis) {
+        this.timeMillis = timeMillis;
+    }
+}
