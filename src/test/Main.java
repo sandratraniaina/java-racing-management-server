15e6c08cddce976be3ac8cdc9e7f619c2a0771e2
sandratraniaina <sandratrarafa@gmail.com>
2024-05-29 10:49:32 +0300

    test: Test new Dao version

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,22 +1,13 @@
 package test;
 
-import java.sql.Connection;
 import java.util.ArrayList;
-
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import dao.Dao;
 import model.Driver;
-import utils.SQLUtils;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-
-        Connection c = ((SQLUtils) context.getBean("sql_utils")).getConnection();
-
-        Dao dao = (Dao) context.getBean("dao");
+        Dao dao = new Dao("config\\context.xml");
         ArrayList<Object> drivers = dao.readAll(new Driver());
         for (Object object : drivers) {
             Driver driver = ((Driver)object);
