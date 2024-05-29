9fe0d9f06359126e2b6b0df9cee3668e1e805bc2
sandratraniaina <sandratrarafa@gmail.com>
2024-05-29 20:23:14 +0300

    feat: Override doGet and doPost method

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,5 +1,22 @@
 package controller;
 
-public class MainController {
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class MainController extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
     
 }
